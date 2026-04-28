fa0767111a9fd956a4bdc0a351e38b2abbc9b74f
Casey Carta <case.ccc23@gmail.com>
2026-04-28 14:12:32 -0400

    Fixed Tab/Button/UI Section Positions.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HCI D26 - Hifi Prototype
+
+[This link](http://localhost:3000/) will be used for accessing the prototype once it is running locally on a given machine.
--- interanimos/app/main/routes.py
+++ interanimos/app/main/routes.py
@@ -2,7 +2,7 @@ from app import db
 from app.main.models import TabType, TabClick
 from app.main import main_blueprint as main
 from flask import render_template, flash, redirect, url_for, request
-from datetime import datetime
+from datetime import datetime, UTC
 import sqlalchemy as sqla
 
 TAB_NAMES = ['File', 'Bones', 'Skin', 'Constraints', 'Testing', 'Controls/Gizmos']
@@ -12,7 +12,25 @@ TAB_NAMES = ['File', 'Bones', 'Skin', 'Constraints', 'Testing', 'Controls/Gizmos
 def index():
     query = sqla.select(TabType)
     tabs = db.session.scalars(query).all()
-    return render_template('index.html', title='Interanimos - Rigging Workspace', tabs=tabs)
+    return render_template('index.html', title='Bones', tabs=tabs)
+
+@main.route('/tab/file')
+def file_tab():
+    query = sqla.select(TabType).where(TabType.name == 'file')
+    file_tab = db.session.scalars(query).first()
+    if file_tab:
+        # Replacement of Deprecated datetime.utcnow() with datetime.now(UTC).
+        # UTC timezone alias from datetime module found from Google AI Overview.
+        db.session.add( TabClick(tab = file_tab,
+                                 occurred_at = datetime.now(UTC)) )
+        db.session.commit()
+    return render_template('file.html', title='File')
+
+@main.route('/not_implemented')
+def not_implemented():
+    flash("The action you selected has not been implemented for this prototype. " \
+          "Please select another option instead.")
+    return redirect(url_for('main.index'))
 
 @main.route('/tab/<tab_name>')
 def switch_tab(tab_name):
